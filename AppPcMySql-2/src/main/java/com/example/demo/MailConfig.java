3f549cab727a08f550523be270639beba928bbfa
FabioBl77 <167991278+FabioBl77@users.noreply.github.com>
2024-06-04 12:27:57 +0200

    Update MailConfig.java

--- AppPcMySql-2/src/main/java/com/example/demo/MailConfig.java
+++ AppPcMySql-2/src/main/java/com/example/demo/MailConfig.java
@@ -18,7 +18,7 @@ public class MailConfig {
         mailSender.setPort(587);
 
         mailSender.setUsername("fabio.bln77@gmail.com");
-        mailSender.setPassword("turh dahu zgjc wgem");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
